6ed60d02f3afc5a422babb308a8efa29f4a0014e
Fabricio Blois <fabriciomaiablois@protonmail.com>
2021-02-24 20:21:57 +0100

    type resolvers with resolver

--- src/main/java/com/example/demo/graphql/resolver/BeerResolver.java
+++ src/main/java/com/example/demo/graphql/resolver/BeerResolver.java
@@ -1,30 +1,44 @@
 package com.example.demo.graphql.resolver;
 
-import com.example.demo.graphql.dataloader.BrandMappedBatchLoader;
-import com.example.demo.graphql.dataloader.MessageMappedBatchLoader;
 import com.example.demo.graphql.response.Beer;
 import com.example.demo.graphql.response.Brand;
 import com.example.demo.graphql.response.Message;
-import com.example.demo.util.DataLoaderUtil;
+import com.example.demo.repository.BrandRepository;
+import com.example.demo.repository.MessageRepository;
 import graphql.kickstart.tools.GraphQLResolver;
-import graphql.schema.DataFetchingEnvironment;
-import org.apache.commons.lang3.ObjectUtils;
+import lombok.RequiredArgsConstructor;
+import ma.glasnost.orika.MapperFacade;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.stereotype.Component;
 
-import java.util.Set;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
 @Component
+@RequiredArgsConstructor
 public class BeerResolver implements GraphQLResolver<Beer> {
 
-    public CompletableFuture<Brand> getBrand(final Beer instance, final DataFetchingEnvironment dfe) {
-        return DataLoaderUtil.load(instance.getBrandId(), BrandMappedBatchLoader.class, dfe);
+    private final TaskExecutor taskExecutor;
+    private final MapperFacade mapperFacade;
+    private final BrandRepository brandRepository;
+    private final MessageRepository messageRepository;
+
+    public CompletableFuture<Brand> getBrand(final Beer instance) {
+        return CompletableFuture.supplyAsync(() -> brandRepository
+                        .findById(instance.getBrandId())
+                        .map(brand -> mapperFacade.map(brand, Brand.class))
+                        .orElse(null),
+                taskExecutor);
     }
 
-    public CompletableFuture<Set<Message>> getMessages(final Beer instance, final DataFetchingEnvironment dfe) {
-        return DataLoaderUtil
-                .load(instance.getId(), MessageMappedBatchLoader.class, dfe)
-                .thenApply(result -> ObjectUtils.defaultIfNull(result, Set.of()));
+    public CompletableFuture<List<Message>> getMessages(final Beer instance) {
+        return CompletableFuture.supplyAsync(() -> messageRepository
+                        .findAllByBeerId(instance.getId())
+                        .stream()
+                        .map(message -> mapperFacade.map(message, Message.class))
+                        .collect(Collectors.toList()),
+                taskExecutor);
     }
 
 }
--- src/main/java/com/example/demo/repository/MessageRepository.java
+++ src/main/java/com/example/demo/repository/MessageRepository.java
@@ -4,10 +4,9 @@ import com.example.demo.domain.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Set;
 
 public interface MessageRepository extends JpaRepository<Message, Integer> {
 
-    List<Message> findAllByBeerIdIn(Set<Integer> beerIds);
+    List<Message> findAllByBeerId(Integer beerIds);
 
 }
